89c12dfb052fe119751c62a0cef0dc608161f373
boucanier <chironjules.78@gmail.com>
2023-04-04 10:29:18 +0200

    Ajout du constructeur de Scenario

--- src/Scenario.java
+++ src/Scenario.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class Scenario {
     ArrayList<Quete> listeQuetes;
 
+    public Scenario() {
+        listeQuetes = new ArrayList<Quete>();
+    }
+
     public String toString() {
         return listeQuetes.size() + " " + listeQuetes;
     }
