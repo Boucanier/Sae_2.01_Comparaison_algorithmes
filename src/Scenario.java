9a8dcc5c16cb80cf864ff5eb5bc01a41a306ebd8
boucanier <chironjules.78@gmail.com>
2023-04-04 10:20:34 +0200

    Ajout de la méthode toString à la classe Scenario

--- src/Scenario.java
+++ src/Scenario.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 
 public class Scenario {
     ArrayList<Quete> listeQuetes;
+
+    public String toString() {
+        return listeQuetes.size() + " " + listeQuetes;
+    }
 }
