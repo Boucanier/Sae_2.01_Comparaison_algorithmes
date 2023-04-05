9fb29c39666e4edc329eb51b7593598c8802c1cd
Matis Rodier <22203556@dinfo.iut-velizy.uvsq.fr>
2023-04-05 10:58:16 +0200

    branche de test pour Quete Matis

--- intellij/sae2_01/src/main/java/modele/Quete.java
+++ intellij/sae2_01/src/main/java/modele/Quete.java
@@ -1,5 +1,6 @@
 package modele;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Quete {
     int numero;
@@ -14,10 +15,37 @@ public class Quete {
         while (scanner.hasNext()) {
             this.numero = scanner.nextInt();
             String posTemp = scanner.next();
+
+            // on prend la valeur suivante dans une string
             String precondTemp = scanner.next();
+            // puis on convertie celle-ci en liste [X,X,X,X] avec X = int
+            precond = stringToListPrecond(precondTemp);
             this.duree = scanner.nextInt();
             this.experience = scanner.nextInt();
             this.intitule = scanner.next();
         }
+    }
+
+    private static int[] stringToListPrecond(String precondition) {
+        /*
+        @Param :
+            precondition (String) :
+        @Returns :
+            precond[int] : la liste qui va devenir le champ contenant les preconditions nécessaire
+         */
+        int [] precond = new int[4];
+        precondition.replace("(", "");
+        precondition.replace(")", "");
+        precondition.replace(" ", "");
+        Scanner scanPrecondition = new Scanner(precondition).useDelimiter(",");
+        int i = 0;
+        while (scanPrecondition.hasNext()) {
+            String extrait = scanPrecondition.next();
+            if (!extrait.equals("")) {
+                precond[i] = Integer.parseInt(extrait);
+                i++;
+            }
+        }
+        return precond;
     }
 }
