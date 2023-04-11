406f68061a7fb08b3400b9d3f3fb43b37bbb594b
Matis Rodier <22203556@dinfo.iut-velizy.uvsq.fr>
2023-04-11 16:59:32 +0200

    réalisation de la méthode stringToListPrecond qui trasnforme la chaine de caractere des preconditions en liste

--- intellij/sae2_01/src/main/java/modele/Quete.java
+++ intellij/sae2_01/src/main/java/modele/Quete.java
@@ -3,49 +3,50 @@ package modele;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Quete {
-    int numero;
-    int[] pos = new int[2];
-    int[] precond = new int[4];
-    int duree;
-    int experience;
-    String intitule;
+    private int numero;
+    private int[] pos = new int[2];
+    private int[] precond = new int[4];
+    private int duree;
+    private int experience;
+    private String intitule;
 
     public Quete(String ligne) {
         Scanner scanner = new Scanner(ligne).useDelimiter("\\|");
         while (scanner.hasNext()) {
             this.numero = scanner.nextInt();
-            String posTemp = scanner.next();
 
-            // on prend la valeur suivante dans une string
+            String posTemp = scanner.next();
+            // System.out.println(posTemp);
+
             String precondTemp = scanner.next();
-            // puis on convertie celle-ci en liste [X,X,X,X] avec X = int
-            precond = stringToListPrecond(precondTemp);
+            stringToListPrecond(precondTemp);
+            // System.out.println(precond[0] + " " + precond[1] + " " + precond[2] + " " + precond[3]);
+
             this.duree = scanner.nextInt();
             this.experience = scanner.nextInt();
             this.intitule = scanner.next();
         }
     }
 
-    private static int[] stringToListPrecond(String precondition) {
-        /*
+
+    private void stringToListPrecond(String precondition) {
+    /*
         @Param :
             precondition (String) :
         @Returns :
             precond[int] : la liste qui va devenir le champ contenant les preconditions nécessaire
-         */
-        int [] precond = new int[4];
-        precondition.replace("(", "");
-        precondition.replace(")", "");
-        precondition.replace(" ", "");
+    */
+        precondition = precondition.replace("(", "");
+        precondition = precondition.replace(")", "");
+        precondition = precondition.replace(" ", "");
         Scanner scanPrecondition = new Scanner(precondition).useDelimiter(",");
         int i = 0;
         while (scanPrecondition.hasNext()) {
             String extrait = scanPrecondition.next();
             if (!extrait.equals("")) {
                 precond[i] = Integer.parseInt(extrait);
-                i++;
             }
+            i++;
         }
-        return precond;
     }
 }
