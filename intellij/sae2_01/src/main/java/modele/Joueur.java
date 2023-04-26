2c37f5f338b71f643b278950d558641949552150
Matis <matisrod91@gmail.com>
2023-04-26 15:19:33 +0200

    ajout de la class joueur

---
+++ intellij/sae2_01/src/main/java/modele/Joueur.java
@@ -0,0 +1,16 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Joueur {
+    private int dureeTotal;
+    private ArrayList<Integer> parcours;
+    private int experience;
+    private int[] pos = new int[2];
+
+    public Joueur() {
+        /*
+         * Constructeur de la classe joueur
+         */
+    }
+}
