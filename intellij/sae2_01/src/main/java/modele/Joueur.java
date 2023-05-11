35673380aeab084a0b360ad0d567e812a1245c1a
Matis R <82202642+matisrod@users.noreply.github.com>
2023-05-11 17:42:46 +0200

    modification du constructeur

--- intellij/sae2_01/src/main/java/modele/Joueur.java
+++ intellij/sae2_01/src/main/java/modele/Joueur.java
@@ -12,5 +12,12 @@ public class Joueur {
         /*
          * Constructeur de la classe joueur
          */
+        dureeTotal = 0;
+        parcours = new ArrayList<>();
+        experience = 0; // il n'a aucune expérience dès sa création
+
+        //le joueur est en (0, 0) dès qu'il apparait
+        pos[0] = 0;
+        pos[1] = 0;
     }
 }
