90baae5403ceb3a3ed4492c9412877f6bb1491be
boucanier <chironjules.78@gmail.com>
2023-05-24 20:41:07 +0200

    Affichage des détails des quêtes avec des labels

--- intellij/sae2_01/src/main/java/vue/VBoxTable.java
+++ intellij/sae2_01/src/main/java/vue/VBoxTable.java
@@ -2,12 +2,15 @@ package vue;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import lectureEcritureFichier.LectureFichierTexte;
 import modele.ConstantesSolutions;
 import modele.ManagerDeQuete;
+import modele.Quete;
 import modele.Scenario;
 
 /**
@@ -25,12 +28,28 @@ public class VBoxTable extends VBox implements ConstantesSolutions {
     public VBoxTable(String parNomFichier) {
         scenario = LectureFichierTexte.lecture(new File("scenarios" + File.separator + parNomFichier));
         for (String solution : SOLUTIONS){
+            VBox vBox = new VBox();
             Label labelSolution = new Label(solution);
+            vBox.getChildren().addAll(labelSolution);
             ManagerDeQuete managerDeQuete = new ManagerDeQuete(scenario);
-            ArrayList<Integer> arrayList = managerDeQuete.niveau1(solution);
-            Label listeSolutions = new Label(arrayList.toString());
+            ArrayList<Quete> arrayQuetes = managerDeQuete.niveau1(solution);
+            Iterator<Quete> itr = arrayQuetes.iterator();
+            while (itr.hasNext()){
+                Quete e = itr.next();
+                HBox hBox = new HBox();
+                Label numero = new Label(String.valueOf(e.getNumero()) + " ");
+                Label nom = new Label(e.getIntitule() + ", ");
+                Label duree = new Label("duree = " + String.valueOf(e.getDuree()) + ", ");
+                Label experience = new Label("xp = " + String.valueOf(e.getExperience()) + " ");
+                int [] precondTab = e.getPrecond();
+                Label precond = new Label("((" + String.valueOf(precondTab[0]) + ", " + String.valueOf(precondTab[1]) + "), (" + String.valueOf(precondTab[2]) + ", " + String.valueOf(precondTab[3]) + ")) ");
+                int [] posTab = e.getPos();
+                Label pos = new Label(String.valueOf(posTab[0]) + ":" + String.valueOf(posTab[1]));
+                hBox.getChildren().addAll(numero, nom, duree, experience, precond, pos);
+                vBox.getChildren().addAll(hBox);
+            }
             this.setId("solution");
-            this.getChildren().addAll(labelSolution, listeSolutions);
+            this.getChildren().addAll(vBox);
         }
     }
 }
