d22925ebb7584f1e0dbec23f254b0f0395240028
boucanier <chironjules.78@gmail.com>
2023-06-02 18:35:21 +0200

    Choix du scenario dans le formulaire

--- intellij/sae2_01/src/main/java/vue/GridPaneFormulaire.java
+++ intellij/sae2_01/src/main/java/vue/GridPaneFormulaire.java
@@ -1,5 +1,8 @@
 package vue;
 
+import java.io.File;
+import java.util.Arrays;
+
 import controleur.Controleur;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -12,13 +15,17 @@ import modele.ConstantesSolutions;
 
 public class GridPaneFormulaire extends GridPane implements ConstantesSolutions{
     public static ComboBox<String> comboBoxSolution;
+    public static ComboBox<String> comboBoxScenario;
     
     public GridPaneFormulaire() {
         Controleur controleur = new Controleur();
 
-        Label choixSolution = new Label("Choix de la solution");
-        comboBoxSolution = peupleComboBox();
+        Label choixScenario = new Label("Choix du scénario");
+        comboBoxScenario = peupleComboBoxScenario();
+        comboBoxScenario.setValue(comboBoxScenario.getItems().get(0));
 
+        Label choixSolution = new Label("Choix de la solution");
+        comboBoxSolution = peupleComboBoxSolution();
         comboBoxSolution.setValue(comboBoxSolution.getItems().get(0));
 
         Button boutonAnnuler = new Button("_Annuler");
@@ -28,17 +35,19 @@ public class GridPaneFormulaire extends GridPane implements ConstantesSolutions{
         
         this.setPadding(new Insets(10));
         this.add(new Separator(), 0, 0, 2, 1);
-        this.add(choixSolution, 0, 1, 2, 1);
-        this.add(comboBoxSolution, 0, 2, 2, 1);
-        this.add(boutonAnnuler, 0, 3, 1, 1);
-        this.add(boutonValider, 1, 3, 1, 1);
+        this.add(choixScenario, 0, 1, 2, 1);
+        this.add(comboBoxScenario, 0, 2, 2, 1);
+        this.add(choixSolution, 0, 3, 2, 1);
+        this.add(comboBoxSolution, 0, 4, 2, 1);
+        this.add(boutonAnnuler, 0, 5, 1, 1);
+        this.add(boutonValider, 1, 5, 1, 1);
 
         this.setAlignment(Pos.CENTER);
         this.setId("titre");
         this.setGridLinesVisible(true);
     }
 
-    private ComboBox<String> peupleComboBox() {
+    private ComboBox<String> peupleComboBoxSolution() {
         ComboBox<String> comboBox = new ComboBox<String>();
         for (String item : SOLUTIONS) {
             comboBox.getItems().add(item);
@@ -48,5 +57,21 @@ public class GridPaneFormulaire extends GridPane implements ConstantesSolutions{
 
     public static String getChoixSolution() {
         return comboBoxSolution.getValue();
+    }
+
+    private ComboBox<String> peupleComboBoxScenario() {
+        ComboBox<String> comboBox = new ComboBox<String>();
+
+        File[] fichiers = new File("scenarios").listFiles();
+        Arrays.sort(fichiers);
+        
+        for (File item : fichiers){
+            comboBox.getItems().add(item.getName());
+        }
+        return comboBox;
+    }
+
+    public static String getChoixScenario() {
+        return comboBoxScenario.getValue();
     }
 }
