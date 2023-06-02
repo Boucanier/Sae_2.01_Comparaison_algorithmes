43b0cb214381ec4195b40f6b568bfbd3c5fe1bfa
boucanier <chironjules.78@gmail.com>
2023-06-02 18:35:45 +0200

    Récupération des données du formulaire

--- intellij/sae2_01/src/main/java/controleur/Controleur.java
+++ intellij/sae2_01/src/main/java/controleur/Controleur.java
@@ -3,6 +3,7 @@ package controleur;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
 import vue.GridPaneFormulaire;
 
 public class Controleur implements EventHandler {
@@ -11,7 +12,7 @@ public class Controleur implements EventHandler {
         if (event.getSource() instanceof Button){
             switch ((((Button) event.getSource()).getAccessibleText())){
                 case "valider":
-                    System.out.println(GridPaneFormulaire.comboBoxSolution.getValue());
+                    System.out.println(GridPaneFormulaire.getChoixScenario() + " " + GridPaneFormulaire.getChoixSolution());
             }
         }
     }
