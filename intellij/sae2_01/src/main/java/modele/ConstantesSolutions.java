718564b3038cc18ffc479f7d052617f75eee00dd
boucanier <chironjules.78@gmail.com>
2023-06-06 14:11:41 +0200

    Correction des noms de solutions

--- intellij/sae2_01/src/main/java/modele/ConstantesSolutions.java
+++ intellij/sae2_01/src/main/java/modele/ConstantesSolutions.java
@@ -4,7 +4,7 @@ package modele;
  * Interface contenant les noms de chaque solution
  */
 public interface ConstantesSolutions {
-    final String [] SOLUTIONS = {"efficace", "exhaustive"};
+    final String [] SOLUTIONS = {"efficace", "exhaustif"};
     final String [] CRITERE = {"dureeTotal", "experience", "parcoursNum", "deplacement"};
     final String [] TRI = {"meilleur", "pire"};
 }
