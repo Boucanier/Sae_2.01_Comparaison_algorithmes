94b289837e326af5461448a363be7644ade750d0
boucanier <chironjules.78@gmail.com>
2023-06-07 18:55:36 +0200

    Bons import

--- intellij/sae2_01/src/main/java/modele/ManagerDeQuete.java
+++ intellij/sae2_01/src/main/java/modele/ManagerDeQuete.java
@@ -1,6 +1,12 @@
 package modele;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * Classe permettant de gérer les quêtes
